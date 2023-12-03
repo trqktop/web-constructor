74f15048826ecab6af513bfb141b555f9b355b01
Shirin Pavel <trqktop@yandex.ru>
2023-12-03 19:32:03 +0300

    add directory

--- my-app/src/components/App/App.tsx
+++ my-app/src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -11,5 +11,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
